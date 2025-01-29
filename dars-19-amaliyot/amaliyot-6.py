81c3a8a0978719fbefeb19b5fe6132a20d8f044a
Elmurod Urinboev <elmurodorinboyev0@gmail.com>
2025-01-29 11:27:44 +0500

    kod yangilandi

--- dars-19-amaliyot/amaliyot-6.py
+++ dars-19-amaliyot/amaliyot-6.py
@@ -1,5 +1,5 @@
 # Yuqoridagi funksiyada y uchun 2 standart qiymatini bering.
 def sonlar_chiq(x , y=2):
-    """Foydalanuvchidan qiymat olib chiqaruvci funksiya."""
+    """Foydalanuvchidan qiymat olib y chi  darajasin chiqaruvci funksiya."""
     print(f"x = {x} ning {y} darajasi {x**y} ga teng")
 sonlar_chiq(5, 3)
